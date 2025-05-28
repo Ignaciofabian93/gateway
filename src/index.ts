ecf0cdabc76772f6bae1048fdd103e205c65adbd
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-28 09:30:05 -0400

    middlewares

--- src/index.ts
+++ src/index.ts
@@ -58,10 +58,12 @@ app.use(
     origin,
     credentials: true,
   }),
-  express.json({ limit: "20mb" }),
-  express.urlencoded({ extended: true, limit: "20mb" }),
-  cookieParser(),
 );
+
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(cookieParser());
+
 app.use("/auth", auth);
 
 app.use(
