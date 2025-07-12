68e3da1ff339c1510e2279904eacd85ea2f24e2f
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-07-11 20:46:09 -0400

    updates

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,29 @@
-export const environment = process.env.ENVIRONMENT ?? "development";
-export const subgraphsURLs = {
-  users: environment !== "development" ? "http://users_subgraph:4001/graphql" : "http://localhost:4001/graphql",
-  products: environment !== "development" ? "http://products_subgraph:4002/graphql" : "http://localhost:4002/graphql",
-  search: environment !== "development" ? "http://search_subgraph:4003/graphql" : "http://localhost:4003/graphql",
+const allowedEnvironments = ["production", "qa", "development"] as const;
+type Environment = (typeof allowedEnvironments)[number];
+
+const env = process.env.ENVIRONMENT;
+export const environment: Environment = allowedEnvironments.includes(env as Environment)
+  ? (env as Environment)
+  : "development";
+
+// Define URLs for each environment
+const urlsByEnv = {
+  development: {
+    users: "http://localhost:4001/graphql",
+    products: "http://localhost:4002/graphql",
+    search: "http://localhost:4003/graphql",
+  },
+  qa: {
+    users: "http://users_subgraph_prod:4001/graphql",
+    products: "http://products_subgraph_prod:4002/graphql",
+    search: "http://search_subgraph_prod:4003/graphql",
+  },
+  production: {
+    users: "http://users_subgraph_prod:4001/graphql",
+    products: "http://products_subgraph_prod:4002/graphql",
+    search: "http://search_subgraph_prod:4003/graphql",
+  },
 };
+
+// Export URLs based on current environment
+export const subgraphsURLs = urlsByEnv[environment];
