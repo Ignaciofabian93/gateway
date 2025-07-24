4c2c734358e989ea4464b28788c8d0e2464c966b
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-07-24 14:46:54 -0400

    ports fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,10 +15,10 @@ const urlsByEnv = {
     transaction: "http://localhost:4004/graphql",
   },
   qa: {
-    users: "http://users_subgraph_qa:4001/graphql",
-    products: "http://products_subgraph_qa:4002/graphql",
-    search: "http://search_subgraph_qa:4003/graphql",
-    transaction: "http://transaction_subgraph_qa:4004/graphql",
+    users: "http://users_subgraph_qa:4101/graphql",
+    products: "http://products_subgraph_qa:4102/graphql",
+    search: "http://search_subgraph_qa:4103/graphql",
+    transaction: "http://transaction_subgraph_qa:4104/graphql",
   },
   production: {
     users: "http://users_subgraph_prod:4001/graphql",
